1fa7aacd6613ab07048036bbffd87302357fa38e
Eric Heikes <eheikes@gmail.com>
2015-06-08 21:28:45 -0500

    Initial commit.

---
+++ toread.js
@@ -0,0 +1,125 @@
+var module = angular.module('toRead', ['ngResource', 'checklist-model']);
+
+module.factory('toreadFactory', function($resource) {
+  return $resource('toreadapi.php');
+});
+
+module.factory('toreadService', function(toreadFactory) {
+  function get(opts) {
+    return toreadFactory.get(opts).$promise;
+  }
+
+  function add(data) {
+    return toreadFactory.save(data).$promise;
+  }
+
+  function remove(idsArray) {
+    return toreadFactory.remove({ 'id[]': idsArray }).$promise;
+  }
+
+  return {
+    get: get,
+    add: add,
+    remove: remove
+  };
+});
+
+module.filter('dasherize', function() {
+  return function(str) {
+    return str.replace(/\s+/g, '-');
+  };
+});
+
+module.controller('addController', function($scope, toreadService) {
+  $scope.data = {};
+  $scope.isSaving = false;
+  $scope.submit = function() {
+    $scope.isSaving = true;
+    toreadService.add($scope.data).then(function() {
+      $scope.data = {};
+      $scope.$emit('refreshList');
+    }).finally(function() {
+      $scope.isSaving = false;
+    });
+  }
+});
+
+module.directive('list', function($timeout, toreadService) {
+  return {
+    restrict: 'E',
+    templateUrl: 'toread.html',
+    link: function(scope, element, attrs) {
+      scope.moment = moment;
+      scope.tags = [];
+      scope.entries = [];
+      scope.info = {};
+      scope.selectedItems = [];
+      scope.actionInProgress = false;
+      scope.offset = 0;
+      scope.limit = 20;
+      scope.tagFilter = null;
+      scope.q = ''; // search query
+
+      var random = function(lo, hi) {
+        return Math.floor(Math.random() * (hi - lo + 1)) + lo;
+      };
+
+      scope.chooseRandom = function() {
+        var chosenIndex = random(0, scope.info.total - 1);
+        var remainder = chosenIndex % scope.limit;
+
+        scope.offset = parseInt(chosenIndex / scope.limit, 10) * scope.limit;
+        scope.showList().then(function() {
+          scope.entries[remainder].highlighted = true;
+          $timeout(function() {
+            $('html,body').animate({
+              scrollTop: $('ul.entries li').eq(remainder).offset().top
+            }, 500);
+          });
+        });
+      };
+
+      scope.toggleTag = function(tagId) {
+        scope.tagFilter = (scope.tagFilter === tagId ? null : tagId);
+        scope.offset = 0;
+        scope.showList();
+      };
+
+      scope.showList = function() {
+        return toreadService.get({
+          q: scope.q,
+          tag: scope.tagFilter,
+          offset: scope.offset,
+          limit: scope.limit
+        }).then(function(response) {
+          scope.tags       = response.tags;
+          scope.entries    = response.links;
+          scope.info       = response.stats;
+          scope.info.total = response.total;
+        });
+      };
+
+      scope.changePage = function(pageOffset) {
+        scope.offset = scope.offset + (scope.limit * pageOffset);
+        if (scope.offset < 0 || scope.offset >= scope.info.total) {
+          scope.offset = 0;
+        }
+        scope.showList();
+      };
+
+      scope.deleteSelected = function() {
+        scope.actionInProgress = true;
+        toreadService.remove(scope.selectedItems).then(function() {
+          scope.selectedItems = [];
+          scope.showList();
+        }).finally(function() {
+          scope.actionInProgress = false;
+        });
+      };
+
+      scope.$on('refreshList', scope.showList);
+
+      scope.showList();
+    }
+  }
+});
---
+++ toread.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en" ng-app="toRead">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>To-Read List</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <style>
+      .stats ins { color: red; text-decoration: none; }
+      .stats del { color: green; text-decoration: none; }
+      .strike { text-decoration: line-through; }
+      .description { white-space: pre-line; margin: 0 0 0 1.2em; }
+      .label { margin-right: 0.25em; }
+      .faded { opacity: 0.5; }
+      .highlight { background: yellow; }
+      ul.entries { margin-left: 0; padding-left: 0; }
+      ul.entries li { list-style: none; padding: 0.15em 0.25em; }
+      #controls > div { margin-bottom: 15px; }
+      #search { width: 100%; }
+      #search input { width: 100%; }
+      #add-new-link { margin-bottom: 15px; }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <h1><a href="">To Read</a></h1>
+          <list></list>
+        </div>
+        <div class="col-md-4">
+          <form id="add-new-link" ng-controller="addController" ng-submit="submit()">
+            <h2>Add New Link</h2>
+            <fieldset ng-disabled="isSaving">
+              <div class="form-group">
+                <label for="new-url" title="Link URL">URL</label>
+                <input id="new-url" type="text" name="url" ng-model="data.url" value="" class="form-control">
+              </div>
+              <div class="form-group">
+                <label for="new-tags" title="Link tags (separate with commas)">Tags</label>
+                <input id="new-tags" type="text" name="tags" ng-model="data.tags" value="" class="form-control">
+              </div>
+              <div class="form-group">
+                <label for="new-description">Description</label>
+                <textarea name="keywords" ng-model="data.keywords" rows="3" cols="50" placeholder="when saving a reference" class="form-control"></textarea>
+              </div>
+              <input type="submit" class="btn btn-default" value="Add">
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular-resource.min.js"></script>
+    <script src="http://vitalets.github.io/checklist-model/checklist-model.js"></script>
+    <script src="toread.js"></script>
+  </body>
+</html>
