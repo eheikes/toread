c2e58826d9bf7273f1f225641c937aea1621ade8
Eric Heikes <eheikes@gmail.com>
2017-04-12 21:36:50 -0500

    Add "s" access key to the first search field.
    fixes #11

--- toread.js
+++ toread.js
@@ -175,6 +175,9 @@ module.directive('list', function($location, $window, $timeout, toreadService) {
 module.directive('controls', function() {
   return {
     restrict: 'E',
+    scope: {
+      hasSearchAccess: '=searchAccess'
+    },
     templateUrl: 'toreadcontrols.html',
     link: function(scope, element, attrs) {}
   };
